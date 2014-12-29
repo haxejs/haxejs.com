00cc7d1cf01845457dcc6bdc7825f039ffa42114
Richard Shi <kmshi_array@yahoo.com>
2014-12-29 16:24:50 +0800

    rewrite by requirejs

--- project/build.js
+++ project/build.js
@@ -1,6 +1,7 @@
-require.config({
-	baseUrl: './',
+({
+	baseUrl: './www/',
     paths: {
+		//exclude modules -- "module":'empty:',
         "angular": 'bower_components/angular/angular',
         "angular-cookies":'bower_components/angular-cookies/angular-cookies',
         "angular-route":'bower_components/angular-route/angular-route',
@@ -13,31 +14,11 @@ require.config({
         "bootstrap":'bower_components/bootstrap/dist/js/bootstrap',
         "jquery":'bower_components/jquery/dist/jquery',
         "ng-grid":'bower_components/ng-grid/ng-grid-2.0.12.debug',
+		"requireLib":"bower_components/requirejs/require",
         "app":"js/app"
     },
-    shim: {
-    	"angular-cookies":{deps: ['angular']},
-    	"angular-route":{deps: ['angular']},
-    	"angular-translate":{deps: ['angular']},
-    	"angular-translate-handler-log":{deps: ['angular-translate']},
-    	"angular-translate-storage-local":{deps: ['angular-translate']},
-    	"angular-translate-storage-cookie":{deps: ['angular-translate']},
-    	"angular-translate-loader-url":{deps: ['angular-translate']},
-    	"angular-translate-loader-static-files":{deps: ['angular-translate']},
-    	"bootstrap":{deps: ['jquery']},
-    	"ng-grid":{deps: ['angular']},
-    	"angular":{exports:'angular',deps: ['jquery']},
-        "jquery":{exports:'jquery'},
-    	"app":{deps: ['angular']}
-    }
-});
 
-require(['angular','jquery',
-	'bootstrap','angular-cookies',
-	'angular-route','angular-translate',
-	'angular-translate-storage-local','angular-translate-storage-cookie',
-	'angular-translate-loader-url','angular-translate-loader-static-files',
-	'angular-translate-handler-log','ng-grid','app'], function(angular) {
-
-		angular.bootstrap(document,["com.haxejs"]);
-});
+	include:"requireLib",
+	name:"js/config",
+	out:"../js/app-compiled.js"
+})
